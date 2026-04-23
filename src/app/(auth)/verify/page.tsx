d6fd50cbb5720eb853818da8c2fc8011e86e1afb
Sanjeevan23 <yogaratnamsanjeevan@gmail.com>
2026-04-23 21:23:16 +0530

    /verify/page.tsx render issue fixed

--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import OtpForm from "../../../components/form/OtpForm";
 
-export default function VerifyPage() {
-  return <OtpForm />;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OtpForm variant="register" />
+    </Suspense>
+  );
 }
